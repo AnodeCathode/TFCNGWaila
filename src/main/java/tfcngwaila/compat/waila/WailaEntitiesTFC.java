b62e6f002a18fa37c9a563be8a8ad040c9b44604
repo_alt <wvk17@yandex.ru>
2020-04-29 16:10:06 +0300

    Animals support

--- src/main/java/tfcngwaila/compat/waila/WailaEntitiesTFC.java
+++ src/main/java/tfcngwaila/compat/waila/WailaEntitiesTFC.java
@@ -3,19 +3,20 @@ package tfcngwaila.compat.waila;
 import java.util.List;
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import net.dries007.tfc.objects.entity.animal.EntityAnimalMammal;
+import net.dries007.tfc.util.calendar.CalendarTFC;
+import net.dries007.tfc.util.calendar.ICalendar;
+import net.dries007.tfc.util.calendar.ICalendarFormatted;
+
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.world.World;
 
 import mcp.MethodsReturnNonnullByDefault;
 import mcp.mobius.waila.api.*;
 import net.dries007.tfc.api.types.IAnimalTFC;
-import net.dries007.tfc.api.types.ICreatureTFC;
-import net.dries007.tfc.objects.entity.EntitiesTFC;
-import net.dries007.tfc.objects.entity.animal.EntityCowTFC;
 
+@SuppressWarnings("unused")
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class WailaEntitiesTFC implements IWailaEntityProvider
@@ -24,74 +25,49 @@ public class WailaEntitiesTFC implements IWailaEntityProvider
     {
         WailaEntitiesTFC dataProvider = new WailaEntitiesTFC();
         registrar.registerBodyProvider(dataProvider, IAnimalTFC.class);
-
-
-
-
-    }
-    @Override
-    public Entity getWailaOverride(IWailaEntityAccessor accessor, IWailaConfigHandler config)
-    {
-        return null;
-    }
-
-    @Override
-    public List<String> getWailaHead(Entity entity, List<String> currenttip, IWailaEntityAccessor accessor, IWailaConfigHandler config)
-    {
-        return null;
     }
 
     @Override
     public List<String> getWailaBody(Entity entity, List<String> currenttip, IWailaEntityAccessor accessor, IWailaConfigHandler config)
     {
-
-        if (entity instanceof EntityCowTFC) currenttip = EntityCowBody(entity, currenttip, accessor, config);
+        IAnimalTFC animal = (IAnimalTFC) entity;
+        boolean familiarized = animal.getFamiliarity() > 0.15f;
+        currenttip.add(new TextComponentTranslation(familiarized ? "familiarized" : "notfamiliarized").getFormattedText());
+        switch (animal.getAge())
+        {
+            case CHILD:
+                currenttip.add(new TextComponentTranslation("childhoodend").getFormattedText()
+                    + ": " + ICalendarFormatted.getTimeAndDate(
+                    ICalendar.TICKS_IN_DAY * (animal.getBirthDay() + animal.getDaysToAdulthood())
+                    , CalendarTFC.CALENDAR_TIME.getDaysInMonth()));
+                break;
+            case OLD:
+                currenttip.add(new TextComponentTranslation("old").getFormattedText());
+                break;
+            case ADULT:
+                if (familiarized)
+                {
+                    if (animal.isReadyToMate())
+                    {
+                        currenttip.add(new TextComponentTranslation("getbusy").getFormattedText());
+                    }
+                    if (animal.isFertilized())
+                    {
+                        NBTTagCompound nbt = accessor.getNBTData();
+                        long pregnancyDate = nbt.getLong("pregnant");
+                        if (pregnancyDate > 0)
+                            currenttip.add(new TextComponentTranslation("pregnancyend").getFormattedText()
+                                + ": " + ICalendarFormatted.getTimeAndDate(
+                                ICalendar.TICKS_IN_DAY * (pregnancyDate + 240), CalendarTFC.CALENDAR_TIME.getDaysInMonth()));
+                    }
+                    if (animal.getGender() == IAnimalTFC.Gender.FEMALE && animal.isReadyForAnimalProduct())
+                    {
+                        currenttip.add(new TextComponentTranslation(
+                            (animal instanceof EntityAnimalMammal) ? "milked" : "haseggs").getFormattedText());
+                    }
+                }
+                break;
+        }
         return currenttip;
     }
-
-    private List<String> EntityCowBody(Entity entity, List<String> currenttip, IWailaEntityAccessor accessor, IWailaConfigHandler config)
-    {
-        EntityCowTFC cow = (EntityCowTFC) entity;
-        NBTTagCompound nbt = accessor.getNBTData();
-
-        float familiarity = Math.max(0.0F, Math.min(1.0F, cow.getFamiliarity()));
-        IAnimalTFC.Gender gender = cow.getGender();
-        if (cow.getPercentToAdulthood() < 1)
-        {
-            currenttip.add(new TextComponentTranslation("baby").getFormattedText());
-        }
-        if (cow.isReadyToMate())
-        {
-            currenttip.add(new TextComponentTranslation("getbusy").getFormattedText());
-        }
-        if (gender == IAnimalTFC.Gender.FEMALE )
-        {
-            currenttip.add(new TextComponentTranslation("female").getFormattedText());
-            if (cow.isReadyForAnimalProduct())
-            {
-                currenttip.add(new TextComponentTranslation("milked").getFormattedText());
-            }
-            if (cow.isFertilized())
-            {
-                currenttip.add(new TextComponentTranslation("pregnant").getFormattedText());
-            }
-        }
-        else
-        {
-            currenttip.add(new TextComponentTranslation("male").getFormattedText());
-        }
-        String foo = cow.getCustomNameTag();
-        return currenttip;
-    }
-
-    @Override
-    public List<String> getWailaTail(Entity entity, List<String> currenttip, IWailaEntityAccessor accessor, IWailaConfigHandler config) {
-        return null;
-    }
-    @Override
-    public NBTTagCompound getNBTData(EntityPlayerMP player, Entity ent, NBTTagCompound tag, World world) {
-        return ent.getEntityData();
-    }
-
-
 }
